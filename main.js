99d32fae7480da95d13208d91bc3c85b1fa9a7c4
BelenEuvrard <beleneuvrard@gmail.com>
2021-07-08 19:34:14 -0300

    desafio 3

--- main.js
+++ main.js
@@ -2,27 +2,84 @@
 alert('Bienvenido');
 let nombre = prompt('Ingrese su nombre');
 let apellido = prompt('Ingrese su apellido');
-alert(`Hola ${nombre} usted fue registrado exitosamente,¡empecemos a gestionar su economia familiar!`);
-let ingresos = Number (prompt('Registre su ingreso Familiar mensual'));
+alert(`Hola ${nombre + ' ' + apellido} usted fue registrado exitosamente,¡empecemos a gestionar su economia familiar!`);
+let ingresos = parseFloat (prompt('Registre su ingreso Familiar mensual'));
 
-
-function resta (num1,num2,num3,num4){
+function resta (num1,num2){
     
-return  num1 - num2 - num3 - num4;
-    
-}
+    return  num1 - num2;
+        
+    }
+   
 
 if (ingresos >= 1 ){
-    let agua = Number ( prompt('Registre su gasto de agua'));
-    let luz = Number ( prompt('Registre su gasto de luz'));
-    let gas = Number ( prompt('Registre su gasto de gas'));
-  
-    let resultado = resta(ingresos,agua,luz,gas);
-    alert(`le quedan ${resultado} para gastar`)
-}
 
-// desafio de arrays y objetos
+    let swap = true
+    while(swap) {
+        let agua = parseFloat ( prompt('Registre su gasto de agua'));
+                   let resultado1 = resta(ingresos,agua);   
+        let luz = parseFloat ( prompt('Registre su gasto de luz'));
+                  let resultado2 = resta(resultado1,luz); 
+        let gas = parseFloat ( prompt('Registre su gasto de gas'));
+                  let resultadoFinal= resta(resultado2,gas); 
+       
+        alert(`le quedan ${resultadoFinal} para gastar`)
+      
+      swap = false;
+    }
+        
+} else {alert('ingrese nuevamente sus datos')}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//pruebas
+/*
 class servicio {
     constructor(nombre,valor){
         this.nombre = nombre;
@@ -30,7 +87,7 @@ class servicio {
 
     }
 }
-
+let resultado = array.reduce( (a,b) => a+b , 0 )
 const servicios = [];
 const servicio1 = new servicio("agua",1500);
 const servicio2 = new servicio("luz",4600);
@@ -44,5 +101,4 @@ console.log(servicios)
 console.log(servicio1)
 console.log(servicio2)
 console.log(servicio3)
-
-
+*/
