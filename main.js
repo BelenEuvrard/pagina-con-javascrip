812f3c553fbe11b726ded4190a7ec90163654424
BelenEuvrard <beleneuvrard@gmail.com>
2021-07-30 22:53:01 -0300

    desafio clase 7y8

--- main.js
+++ main.js
@@ -1,9 +1,108 @@
 
 alert('Bienvenido');
 let nombre = prompt('Ingrese su nombre');
+localStorage.setItem('Nombre', nombre);
 let apellido = prompt('Ingrese su apellido');
-alert(`Hola ${nombre + ' ' + apellido} usted fue registrado exitosamente,¡empecemos a gestionar su economia familiar!`);
-let ingresos = parseFloat (prompt('Registre su ingreso Familiar mensual'));
+localStorage.setItem('Apellido', apellido);
+//alert(`Hola ${nombre + ' ' + apellido} usted fue registrado exitosamente,¡empecemos a gestionar su economia familiar!`);
+//let ingresos = parseFloat (prompt('Registre su ingreso Familiar mensual'));
+//localStorage.setItem('IngresoFamiliar', ingresos);
+
+
+// DESAFIO CLASE 7 Y 8
+let saludo = document.getElementById("saludo");
+let parrafo = document.createElement("p");
+parrafo.innerHTML = `Hola ${nombre + ' ' + apellido} usted fue registrado exitosamente,¡empecemos a gestionar su economia familiar!`;
+saludo.appendChild(parrafo);
+
+
+
+
+/*
+
+class Ingreso {
+    constructor(ingresoFamiliar){
+
+    this.ingresoFamiliar = ingresoFamiliar;
+
+   }
+}
+
+class Servicio {
+    constructor(servicioApagar,importeServicio){
+        this.servicioApagar = servicioApagar;
+        this.importeServicio = importeServicio;
+
+    }
+}
+
+class EventosDelUsuario {
+
+    agregarIngreso(ingreso){
+        const litsingreso = document.getElementById("ingresoServicio");
+        const elementIngreso =document.createElement("div");
+        elementIngreso.innerHTML = 
+        `
+        <div class="card text-center mb-4">
+            <div class="card-body">
+                <strong>Ingreso Familiar</strong>: ${ingreso} -
+                
+                <a href="#" class="btn btn-danger" name="delete">Delete</a>
+            </div>
+        </div>
+    `;
+             litsingreso.appendChild(elementIngreso);
+
+    }
+
+    agregarServicio(){
+
+    }
+
+    resetearFormulario(){
+
+    }
+
+    eliminarIngreso(){
+
+    }
+
+    eliminarServicio(){
+
+    }
+    mensajeServicio(){
+
+    }
+
+}
+
+ 
+//DOM Eventos
+document.getElementById("form-servicios" )
+        .addEventListener("submit", function(e) { 
+            const ingresoFamiliar = document.getElementById("ingresoFamiliar").value;
+            const servicioApagar = document.getElementById("inputGroupSelect04").value;
+            const importeServicio = document.getElementById("importeServicio").value; 
+
+            console.log(ingresoFamiliar,servicioApagar,importeServicio)
+
+            const ingreso = new Ingreso(ingresoFamiliar);
+            const servicio = new Servicio(servicioApagar,importeServicio);
+            console.log(servicio,ingreso)
+
+            const evento = new EventosDelUsuario();
+            evento.agregarIngreso(ingreso)
+
+            e.preventDefault();
+        
+        
+        }) ;   
+     
+      
+      
+
+      
+  
 
 function resta (num1,num2){
     
@@ -31,18 +130,11 @@ if (ingresos >= 1 ){
 } else {alert('ingrese nuevamente sus datos')}
 
 
-class servicio {
-    constructor(nombre,valor){
-        this.nombre = nombre;
-        this.valor =valor;
-
-    }
-}
-
 const servicios = [];
-const servicio1 = new servicio("agua",1500);
-const servicio2 = new servicio("luz",4600);
-const servicio3 = new servicio("gas",2500);
+
+const servicio1 = new Servicio("agua",1500);
+const servicio2 = new Servicio("luz",4600);
+const servicio3 = new Servicio("gas",2500);
 servicios.push(servicio1);
 servicios.push(servicio2);
 servicios.push(servicio3);
@@ -53,73 +145,53 @@ console.log(servicio1)
 console.log(servicio2)
 console.log(servicio3)
 
+localStorage.setItem('serviciosIngresados',JSON.stringify(servicios));
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//pruebas
-/*
-class servicio {
-    constructor(nombre,valor){
-        this.nombre = nombre;
-        this.valor =valor;
-
-    }
-}
-let resultado = array.reduce( (a,b) => a+b , 0 )
-const servicios = [];
-const servicio1 = new servicio("agua",1500);
-const servicio2 = new servicio("luz",4600);
-const servicio3 = new servicio("gas",2500);
-servicios.push(servicio1);
-servicios.push(servicio2);
-servicios.push(servicio3);
-
-console.log(servicios)
-
-console.log(servicio1)
-console.log(servicio2)
-console.log(servicio3)
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
